574db0d987c597e268afa4fb4259480626cdef68
Usello <91811059+Usello@users.noreply.github.com>
2022-04-19 08:57:38 -0300

    Update index.php

--- tai_2022/index.php
+++ tai_2022/index.php
@@ -17,6 +17,26 @@
     foreach ($result as $item){
         echo "ID".$item['id']." Nome: ".$item['nome'] ."<br>";
     }
+
+    if (!empty($_POST)){
+        echo "Salvar";
+        var_dump($_POST);
+        $objBD->inserir($_POST);
+        
+    }
 ?>
+<h2>Formulário cliente</h2>
+<form action="index.php" method="post">
+    <label>Nome</label>
+    <input type="text" name="nome"/>
+
+    <label>Telefone</label>
+    <input type="text" name="telefone"/>
+
+    <label>CPF</label>
+    <input type="text" name="cpf"/>
+
+    <input type="submit" value="Salvar"/>
+</form>
 </body>
 </html>
